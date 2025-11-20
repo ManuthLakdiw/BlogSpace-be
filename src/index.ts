63fb6151b6a0f9edc77acf9bcac9a2024e9fdd5b
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-11-20 13:14:36 +0530

    added

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,8 @@ mongoose
     })
 
 
-
-app.listen(process.env.PORT, () => {
-    console.log("Server is running")
-})
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.POR}`);
+    });
+}
